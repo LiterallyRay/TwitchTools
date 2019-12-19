a432c949e5d4bb6655f91e3721073dcbe85bcab2
Ray A. Zuniga <29696391+LiterallyRay@users.noreply.github.com>
2019-12-19 15:01:26 -0800

    Create randomLandingSpot.js

---
+++ randomLandingSpot.js
@@ -0,0 +1,18 @@
+var myArray = ["Nuketown", 
+"Construction Site", 
+"Estates", 
+"Array", 
+"Firing Range", 
+"Train Station", 
+"Hydro Dam", 
+"Asylum", 
+"Fracking Tower", 
+"Cargo Docks", 
+"Factory", 
+"Turbine", 
+"Lighthouse", 
+"Turbine", 
+"Rivertown", 
+"Ghost Town"];
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+return randomItem;
