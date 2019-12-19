52c88c9f1891d46ee7b28cab726bc62c750d92c3
Ray A. Zuniga <29696391+LiterallyRay@users.noreply.github.com>
2019-12-19 12:57:07 -0800

    Create randomLandingSpot.php

---
+++ randomLandingSpot.php
@@ -0,0 +1,10 @@
+<?php
+//list of landing spots
+$arr = array("Nuketown", "Construction Site", "Estates", "Array", "Firing Range", "Train Station", "Hydro Dam", "Asylum", "Fracking Tower", "Cargo Docks", "Factory", "Turbine", "Lighthouse", "Turbine", "Rivertown", "Ghost Town");
+
+$randIndex = array_rand($arrX);
+
+//return random location
+echo $arr[$randIndex];
+
+?>
